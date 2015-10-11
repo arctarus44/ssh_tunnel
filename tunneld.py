be4da5bdc036cedd05b539b397830f0085d8a3d0
arctarus <arthur.dewarumez@gmail.com>
2015-10-11 18:02:47 +0200

    Add check if the request fifo is empty.

--- tunneld.py
+++ tunneld.py
@@ -8,7 +8,7 @@ import base64
 CONTENT_TYPE = "Content-type"
 TXT_HTML = "text/html"
 
-ssh_query = None
+fifo_query = None
 
 class TunnelHTTPHandler(http.server.SimpleHTTPRequestHandler):
 
@@ -19,36 +19,47 @@ class TunnelHTTPHandler(http.server.SimpleHTTPRequestHandler):
 
 	def do_GET(self):
 		"""Respond to a GET request."""
-		global ssh_query
-		self.send_response(200)
-		self.send_header("Content-type", "text/html")
-		self.end_headers()
-		query = base64.b64encode(ssh_query.get())
-		self.wfile.write(query)
+		global fifo_query
+		if fifo_query.empty():
+			self.send_response(404)
+			self.send_header("Content-type", "text/html")
+			self.end_headers()
+			self.wfile.write(b"I am so sorry :(")
+		else:
+			self.send_response(200)
+			self.send_header("Content-type", "text/html")
+			self.end_headers()
+			query = base64.b64encode(fifo_query.get())
+			self.wfile.write(query)
 
 
 def httpd():
 	server_class = http.server.HTTPServer
 	httpd = server_class(("localhost", 8000), TunnelHTTPHandler)
+	print("Starting the HTTP server thread")
 	httpd.serve_forever()
 
-def sshd():
-	global ssh_query
-	sockt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sockt.bind(('', 2222))
-	sockt.listen(10)
+def listend():
+	global fifo_query
+	print("Starting the query listener thread")
+	socket_query = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	socket_query.bind(('', 2222))
+	socket_query.listen(10)
 	while True:
-		client, address = sockt.accept()
-		print("{0} connected".format(address))
+		client, info = socket_query.accept()
+		print("{0} connected".format(info))
+		# maybe a while true for client.recv is a good idea.
 		query = client.recv(255)
-		ssh_query.put(query)
+		fifo_query.put(query)
 
 
 if __name__ == "__main__":
-	ssh_query = Queue()
+	# Todo when thread quit, close cleany the socket.
+	# Add management of the response.
+	fifo_query = Queue()
 
 	httpd_thread = threading.Thread(None, httpd, name="HTTPD-thread")
 	httpd_thread.start()
 
-	sshd_thread = threading.Thread(None, sshd, name="SSHD-thread")
+	sshd_thread = threading.Thread(None, listend, name="Listend-thread")
 	sshd_thread.start()
