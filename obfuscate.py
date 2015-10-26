60ca556f0c65d315b21330018eca9adabfcfcc31
arctarus <arthur.dewarumez@etudiant.univ-lille1.fr>
2015-10-26 17:42:10 +0100

    Added class to implementation (de)obfuscation scheme. Need to add some methods

--- obfuscate.py
+++ obfuscate.py
@@ -14,18 +14,35 @@ class Obfuscate:
 	__url_img  = [".jpg", ".bmp"]
 	__url_text = ["toto.html", "fooo.html", "barr.php"]
 	__end_url = __url_img + __url_text
+	__markers = ["p", "a", "h2"]
 
-	@classmethod
-	def random_url(cls):
+	def __init__(self):
+		self.mapping = {}
+		i = 0
+		for end in self.__url_text:
+			self.mapping[end] = self.__markers[i]
+			i+=1
+
+	def __random_url(self):
 		"""Generate a random url."""
 		begin = ''.join(random.choice(string.ascii_lowercase +
 		                              string.digits + "/")
 		                for _ in range(URL_SIZE))
 
-		end = random.choice(cls.__end_url)
+		end = random.choice(self.__end_url)
 
-		if end in cls.__url_img and begin[-1] == "/":
+		if end in self.__url_img and begin[-1] == "/":
 			begin += "".join(random.choice(string.ascii_lowercase
 			                               + string.digits)
 			                 for _ in range(SUP_SIZE))
 		return begin + end
+
+	def obfuscate(self, url, data):
+		"""Obfuscate data using url to determine wich kind of obfuscation to
+		used."""
+		pass
+
+	def deobfuscate(self, url, data):
+		"""Deobfuscate the data using url to determine wich kind of
+		deobfuscation to use."""
+		pass
