f286d8d930e411fc7bb6213d0cb22106277ae8d8
arctarus <arthur.dewarumez@etudiant.univ-lille1.fr>
2015-10-27 14:29:53 +0100

    Correction.

--- obfuscate.py
+++ obfuscate.py
@@ -11,15 +11,17 @@ SUP_SIZE = 5
 # todo find a better name for the following class
 class Obfuscate:
 
-	__metaclass__ = abc.ABCMeta
-
-	# end of an url
 	__url_img  = [".jpg", ".gif", ".png"]
 	__url_text = ["toto.html", "fooo.html", "barr.php"]
-	__end_url = __url_img + __url_text
+	__end_url = __url_img + __url_text # end of an url
+	__element = ["span", "a", "i"] # every markers used to store data
 
 	def __init__(self):
-		pass
+		self.mapping = {}
+		i = 0
+		for end in self.__url_text:
+			self.mapping[end] = self.__element[i]
+			i+=1
 
 	def random_url(self):
 		"""Generate a random url."""
@@ -35,46 +37,33 @@ class Obfuscate:
 			                 for _ in range(SUP_SIZE))
 		return begin + end
 
-	@abc.abstractmethod
 	def obfuscate(self, url, data):
 		"""Obfuscate data using url to determine wich kind of obfuscation to
 		used."""
+		if url[-3:] in self.__url_img:
+			return self.__obfuscate_image(url, data)
+		if url[-9:] in self.__url_text(url, data):
+			return self.__obfuscate_text(url, data)
 		return
 
-	@abc.abstractmethod
 	def deobfuscate(self, url, data):
 		"""Deobfuscate the data using url to determine wich kind of
 		deobfuscation to use."""
 		return
 
-class ObfuscateImage(Obfuscate):
-
-	__JPG = "jpg"
-	__PNG = "png"
-	__GIF = "gif"
-	__headers = {__GIF: b'GIF89a',
-	             __PNG: b'\x89PNG\r\n\x1a',
-	             __JPG: b'\xff\xd8\xff\xe0\x00\x10JFIF\x00\x01\x01\x00\x00\x01\x00\x01\x00\x00\xff\xfe\x00;CREATOR: gd-jpeg v1.0 (using IJG JPEG v80)'}
-
-
-	def obfuscate(self, url, data):
+	def __obfuscate_image(self, url, data):
 		file_type = url[-3:]
-		print(file_type)
 		header = self.__headers[file_type]
 		return header + data
 
-	def deobfuscate(self, url, data):
+	def __deobfuscate__image(self, url[-3:], data):
 		file_type = url[-3:]
 		header = self.__headers[file_type]
 		content = data.replace(header, "".encode())
 		return content
 
-# class ObfuscateText(Obfuscate):
+	def __obfuscate_text(self, url, data):
+		pass
 
-	# __markers = ["p", "a", "h2"] # every markers used to store data
-	# def __init__(self):
-	# 	self.mapping = {}
-	# 	i = 0
-	# 	for end in self.__url_text:
-	# 		self.mapping[end] = self.__markers[i]
-	# 		i+=1
+	def __deobfuscate__text(self, url, data):
+		pass
