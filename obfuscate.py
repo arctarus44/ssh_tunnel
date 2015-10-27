452f72afa1d26809b9238c03fe41085b0c7aa87d
arctarus <arthur.dewarumez@gmail.com>
2015-10-28 00:44:21 +0100

    Added a class to obfuscate content into a random HTML page.

--- obfuscate.py
+++ obfuscate.py
@@ -7,6 +7,63 @@ import abc
 
 URL_SIZE = 15
 SUP_SIZE = 5
+SPLIT = 10						# split every 10har
+
+class HTMLGenerator:
+
+	__MAX_DEPTH = 7
+	__BLOCk_MARKER = ["div","p", "h1" ,"h2", "h3"]
+	__PROBA_BLOCk = 75			# 0.75 of chance to have a bloc
+
+	def __init__(self, marker, payload):
+		self.__MARKER = marker
+		self.__payload = [payload[i:i+SPLIT]
+		                  for i in range(0, len(payload), SPLIT)]
+		self.__payload_used = 0
+		self.__depth = 1
+
+	def __use_block(self):
+		rand = random.randint(0,100)
+		if rand > self.__PROBA_BLOCk:
+			return False
+		else:
+			return True
+
+	def random_page(self):
+		page = '<html><body>'
+		page += self.__random_body()
+		page += '</body></html>'
+		return page
+
+	def __random_body(self):
+		body = ""
+		while self.__payload_used < len(self.__payload):
+			if self.__use_block():
+				body += self.__random_block()
+			else:
+				body += '<div>'
+				body += self.__gen_inline()
+				body += '</div>'
+		return body
+
+	def __random_block(self):
+		block = ""
+		if self.__use_block() and self.__depth < self.__MAX_DEPTH:
+			marker = random.choice(self.__BLOCk_MARKER)
+			self.__depth += 1
+			block += "<" + marker + ">"
+			block += self.__random_block()
+			block +=  "</" + marker + ">"
+		else:
+			block += self.__gen_inline()
+		return block
+
+	def __gen_inline(self):
+		inline = "<" + self.__MARKER + ">"
+		inline += self.__payload[self.__payload_used]
+		self.__payload_used += 1
+		inline += "</" + self.__MARKER + ">"
+		return inline
 
 # todo find a better name for the following class
 class Obfuscate:
@@ -44,7 +101,7 @@ class Obfuscate:
 			return self.__obfuscate_image(url, data)
 		if url[-9:] in self.__url_text(url, data):
 			return self.__obfuscate_text(url, data)
-		return
+		raise ValueError("No obfuscation scheme for this url")
 
 	def deobfuscate(self, url, data):
 		"""Deobfuscate the data using url to determine wich kind of
